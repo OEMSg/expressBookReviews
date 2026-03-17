576b39a7ac9a1b2e6164d5feef55ec6cb98b2355
OEMSg <omareduardo.moso@gmail.com>
2026-03-17 15:22:32 -0400

    get books by isbn using promises

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -45,13 +45,30 @@ public_users.get('/', async function (req, res) {
     }
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn', function (req, res) {
-    // Retrieve the ISBN from the request parameters
+// Task 11: Get book details based on ISBN using async-await and Promises
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
 
-    // Send the corresponding book's details
-    return res.status(200).send(books[isbn]);
+    try {
+        // Create a promise that fetches the book by ISBN
+        const getBookByISBN = new Promise((resolve, reject) => {
+            const book = books[isbn];
+            if (book) {
+                resolve(book);
+            } else {
+                reject(new Error("Book not found"));
+            }
+        });
+
+        // Wait for the promise to resolve
+        const fetchedBook = await getBookByISBN;
+
+        // Send the corresponding book's details
+        return res.status(200).send(JSON.stringify(fetchedBook, null, 4));
+    } catch (error) {
+        // Catch any errors (like the book not being found)
+        return res.status(404).json({ message: error.message });
+    }
 });
 
 // Get book details based on author
